46ce7e68802a65e77903fd6b9eeaa86300a5f7d2
Youjack <iamjasonlin@163.com>
2025-10-10 14:32:38 +0800

    Minor fixes

--- src/SIDISStructFunc/InEW.jl
+++ src/SIDISStructFunc/InEW.jl
@@ -13,7 +13,7 @@ const _rtol = 5e-3
 
 "Inverse-Error Weighting, assuming uniform power corrections to all structure functions."
 function F_InEW(M, F_tmd::Function, F_coll::Function, xB, Q², zh, qT², μ², rtol=_rtol,
-        nΔ=0, Wpow=1, Wnorm=1, Wcut=1)::Float64
+        nΔ=0, Wpow=1, Wnorm=1, Wcut=2)::Float64
     qT, Q = √qT², √Q²
     W() = Wnorm * F_tmd( xB, Q², zh, qT², μ², rtol)
     Z() = F_coll(xB, Q², zh, qT², μ², rtol)
@@ -21,14 +21,14 @@ function F_InEW(M, F_tmd::Function, F_coll::Function, xB, Q², zh, qT², μ², r
     Δ_Z = (M/qT)^2 * ( 1 + 1/4*log(Q²/qT²+1)^2 )
     Δ_Γ = Δ_W * Δ_Z / √( Δ_W^2 + Δ_Z^2 )
     Γ = ( (qT < Wcut*Q ? Δ_Z^2 * W() : 0)
-        + (qT/Q > 0.01 ? Δ_W^2 * Z() : 0) )/
+        + (qT²/Q² > SIDISXSec.Coll.qT²oQ²_low ? Δ_W^2 * Z() : 0) )/
         ( Δ_W^2 + Δ_Z^2 )
     return ( 1 + nΔ * Δ_Γ ) * Γ
 end
 
 """
     get_sf_InEW(M, sf_tmd::SidisStructFunc, sf_coll::SidisStructFunc;
-        nΔ=0, λ=1, κ=1)::SidisStructFuncs
+        nΔ=0, Wpow=1, Wnorm=1, Wcut=2)::SidisStructFuncs
 
 Get structure functions with inverse-error weighting.
 - The error `nΔ * ΔΓ` is added to the functions.
@@ -37,7 +37,7 @@ Get structure functions with inverse-error weighting.
 - `Wcut` is a factor to tune the cutoff in `qT` of the TMD term.
 """
 function get_sf_InEW(M, sf_tmd::SidisStructFunc, sf_coll::SidisStructFunc;
-        nΔ=0, Wpow=1, Wnorm=1, Wcut=1)::SidisStructFunc
+        nΔ=0, Wpow=1, Wnorm=1, Wcut=2)::SidisStructFunc
     InEWargs = (nΔ, Wpow, Wnorm, Wcut)
     return SidisStructFunc(
         (Fargs...) -> F_InEW(M, sf_tmd.FUUL,      sf_coll.FUUL,      Fargs..., InEWargs...),
