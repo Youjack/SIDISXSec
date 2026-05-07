ebc148945c6302cf07dff8bd0f77258b357645ad
Youjack <iamjasonlin@163.com>
2026-05-07 20:32:45 +0800

    remove TMDGrid functionality

--- src/QCDData.jl
+++ src/QCDData.jl
@@ -1,17 +1,13 @@
 module QCDData
 
 using SpecialFunctions: beta
-using LaTeXStrings, Printf
+using LaTeXStrings
 using cLHAPDF
-using Interpolations
 
 export num_quark, quark_name, quark_charge, quark_code, quark_code2idx
 export iso_code, iso_list
 export get_pdf_toy
 export get_qcd_data, get_αs, get_qcd_density
-
-export TMDGrid, TMDGridFloat
-export write_tmdgrid, read_tmdgrid, interpolate_tmdgrid
 
 const num_quark = 10
 const quark_name     =     ( L"u"      , L"\bar{u}", L"d"      , L"\bar{d}", L"s"      , L"\bar{s}", L"c"      , L"\bar{c}" , L"b"      , L"\bar{b}" )
@@ -88,124 +84,6 @@ function get_qcd_density(qcd_data::Ptr{Cvoid}, id::Integer, μ²::Real,
         +    Z  * xfxQ2(qcd_data, (-1)^conj *          id,  x, μ²)
         + (A-Z) * xfxQ2(qcd_data, (-1)^conj * iso_code(id), x, μ²)
         )/A
-end
-
-#= TMDGrid ========================================================================================#
-
-const TMDGRID_PATHS = split(ENV["TMDGRID_PATH"], ":")
-const VarGrid = @NamedTuple{name::String, type::String, start::Float64, stop::Float64, length::Int}
-const TMDGridFloat = Float32
-
-"""
-Grid to store TMD data.
-- `desc` is a description string.
-- `vars = [(name::String, type::String, start::Float64, stop::Float64, length::Int), ...]`
-- `values` is a multi-dimensional array. It mimics the function `values(var1, var2, ...)::TMDGridFloat`.
-"""
-struct TMDGrid
-    desc :: String
-    vars :: Vector{VarGrid}
-    values
-    function TMDGrid(vars, values::AbstractArray, desc::String="No description.")
-        size_values = size(values)
-        if length(vars) ≠ length(size_values)
-            throw(ErrorException("length(vars) ≠ length(size(values))"))
-        end
-        for n ∈ eachindex(vars)
-            if vars[n].length ≠ size_values[n]
-                throw(ErrorException("vars[n].length ≠ size(values)[n]"))
-            end
-        end
-        if eltype(values) ≡ TMDGridFloat
-            new(desc, vars, values)
-        else
-            new(desc, vars, TMDGridFloat.(values))
-        end
-    end
-end
-Base.show(io::IO, tmdgrid::TMDGrid) = print(io,
-    "TMDGrid with vars: ", join([var_grid.name for var_grid ∈ tmdgrid.vars], ", ")
-)
-
-const _sigdigits = 8
-round2str(x) = Printf.format(Printf.Format("%$(_sigdigits+8).$(_sigdigits-1)E"), x)
-
-function write_tmdgrid(name::String, tmdgrid::TMDGrid; path=nothing)
-    if isnothing(path) path = TMDGRID_PATHS[1] end
-    open(joinpath(path, name*".tmdgrid"), "w") do io
-        # write description
-        write(io, tmdgrid.desc*"\n")
-        # write vars
-        for var_grid ∈ tmdgrid.vars
-            write(io, rpad(var_grid.name, 10))
-            write(io, ":")
-            write(io, lpad(var_grid.type, 10))
-            write(io, round2str(var_grid.start))
-            write(io, round2str(var_grid.stop))
-            write(io, lpad("$(var_grid.length)", 5))
-            write(io, "\n")
-        end
-        # write values as binaries (customizable `TMDGridFloat`?)
-        write(io, "values($TMDGridFloat):\n")
-        write(io, tmdgrid.values)
-    end
-    return nothing
-end
-
-function read_tmdgrid(name::String)::Union{TMDGrid,Nothing}
-    filename = ""
-    for path ∈ TMDGRID_PATHS
-        _filename = joinpath(path, name*".tmdgrid")
-        if isfile(_filename)
-            filename = _filename
-            break
-        end
-    end
-    if filename == ""
-        @warn "TMDGrid \"$name\" not found, will be 0."
-        return nothing
-    end
-    open(filename, "r") do io
-        desc = readline(io)
-        split_line::Vector{SubString{String}} = Vector{SubString{String}}(undef, 0)
-        # read vars
-        vars = Vector{VarGrid}(undef, 0)
-        var_lengths = Vector{Int}(undef, 0)
-        while true
-            line = readline(io)
-            if startswith(line, "values") break end
-            split_line = split(line)
-            var_grid = (
-                name = split_line[1], type = split_line[3],
-                start  = parse(Float64, split_line[4]),
-                stop   = parse(Float64, split_line[5]),
-                length = parse(Int,     split_line[6]))
-            push!(vars, var_grid)
-            push!(var_lengths, var_grid.length)
-        end
-        # read values as binaries (customizable `TMDGridFloat`?)
-        values_size = Tuple(var_lengths)
-        values_ndims = length(values_size)
-        values = Array{TMDGridFloat,values_ndims}(undef, values_size)
-        read!(io, values)
-        return TMDGrid(vars, values, desc)
-    end
-end
-
-function get_node(x, var_grid::VarGrid)::Float64
-    xl, xu, L = var_grid.start, var_grid.stop, var_grid.length
-    if     var_grid.type == "lin"
-        return round(( (xu-x) + L * (x-xl) )/ (xu-xl), sigdigits=_sigdigits)
-    elseif var_grid.type == "log"
-        return round(( log(xu/x) + L * log(x/xl) )/ log(xu/xl), sigdigits=_sigdigits)
-    else
-        throw(ErrorException("get_node: unkown VarGrid type `$(var_grid.type)`."))
-    end
-end
-function interpolate_tmdgrid(tmdgrid::Union{TMDGrid,Nothing})::Function
-    if isnothing(tmdgrid) return (xs...) -> 0.0 end
-    itp = interpolate(tmdgrid.values, BSpline(Cubic(Line(OnGrid()))))
-    return (xs...) -> itp((get_node(xs[n], tmdgrid.vars[n]) for n ∈ eachindex(xs))...)
 end
 
 end # module
